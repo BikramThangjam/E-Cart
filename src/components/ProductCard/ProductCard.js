e52ff4a4dd4eab001b4fd49e5793c8c134781736
Bikram <tbsingh@gmail.com>
2023-01-06 09:43:48 +0530

    Added code that redirect the page to Product Detail component when clicked on product image

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -9,15 +9,17 @@ const ProductCard = (props) => {
 
     return (
         <div className="productItem mb-3">
-            <Link className="card card-main position-relative" to={`/products/${product.id}`}>
-                <img src={product.image} className="card-img" alt="..."/>
+            <div className="card card-main position-relative">
+                <Link className="card-img-link" to={`/products/${product.id}`}>
+                    <img src={product.image} className="card-img" alt="..."/>
+                </Link>
                 <div className="card-body">
                     <h6 className="text-center lead fs-6">{product.title}</h6>
                     <h3 className="text-center display-6 fs-3">${product.price}</h3>
                     {/* <p className="card-text ">{product.description}</p> */}
                     <button className=" mx-auto addCart-btn position-absolute bottom-0 start-50 translate-middle-x mb-3 " onClick={()=>dispatch(add(product))}>Add To Cart</button>
                 </div>
-            </Link>
+            </div>
         </div>
     )
 }
